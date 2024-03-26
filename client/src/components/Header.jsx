e47dfd4fef43d26ef537d9c2f9b47f125f7fae7d
Ken-Staples <kenstaples@ymail.com>
2024-03-26 06:30:37 -0600

    Updated header component with User data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, NavbarCollapse, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, NavbarCollapse, TextInput } from 'flowbite-react';
 import React from 'react';
-import { Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector(state => state.user);
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-md sm:text-xl font-bold dark:text-white'>
@@ -26,30 +28,54 @@ export default function Header() {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button outline >
-            Sign In
-          </Button>
-        </Link>
-        <Navbar.Toggle/>
-      </div>
+        {currentUser ? (
+          <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt='user' img={currentUser.profilePicture} rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className='block text-sm'>@{currentUser.username}</span>
+            <span className='block text-sm font-medium truncate'>
+              {currentUser.email}
+            </span>
+          </Dropdown.Header>
+          <Link to={'/dashboard?tab=profile'}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+
+        </Dropdown>
+        ) :
+          (
+            <Link to='/sign-in'>
+              <Button outline >
+                Sign In
+              </Button>
+            </Link>)
+        }
+        <Navbar.Toggle />
+      </div >
       <Navbar.Collapse>
-        <Navbar.Link active ={path === "/"} as={'div'}>
+        <Navbar.Link active={path === "/"} as={'div'}>
           <Link to='/'>
             Home
           </Link>
         </Navbar.Link>
-        <Navbar.Link active ={path === "/about"} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={'div'}>
           <Link to='/about'>
             About
           </Link>
         </Navbar.Link>
-        <Navbar.Link active ={path === "/projects"} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={'div'}>
           <Link to='/projects'>
             Projects
           </Link>
         </Navbar.Link>
       </Navbar.Collapse>
-    </Navbar>
+    </Navbar >
   );
 }
