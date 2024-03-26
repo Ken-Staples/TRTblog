85ac6c417db0dc51e3b915799ed816df09f6f6e4
Ken-Staples <kenstaples@ymail.com>
2024-03-25 19:55:17 -0600

    Added redux persist

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,6 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import { BsFacebook, BsInstagram, BsTwitterX } from 'react-icons/bs';
+import { BsFacebook, BsInstagram, BsTiktok, BsTwitterX, BsYoutube } from 'react-icons/bs';
 export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
@@ -13,21 +13,28 @@ export default function FooterCom() {
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
-              <Footer.Title title='About' />
+              <Footer.Title title='Links' />
               <Footer.LinkGroup col>
-                <Footer.Link
+              <Footer.Link
                   href='#'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  TRT's Blog Mission
+                  Store
                 </Footer.Link>
                 <Footer.Link
                   href='#'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Resources
+                  Help
+                </Footer.Link>
+                <Footer.Link
+                  href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  FAQ
                 </Footer.Link>
                 <Footer.Link
                   href='/about'
@@ -36,19 +43,32 @@ export default function FooterCom() {
                 >
                   About Us
                 </Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.youtube.com'
+                  href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  YouTube
+                  Resources
                 </Footer.Link>
-                <Footer.Link href='#'>Instagram</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Contact us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  E-mail
+                </Footer.Link>
+                <Footer.Link href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                    Messenger</Footer.Link>
+                    <Footer.Link href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                    X</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
@@ -68,9 +88,11 @@ export default function FooterCom() {
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsYoutube}/>
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitterX}/>
+            <Footer.Icon href='#' icon={BsTiktok}/>
           </div>
         </div>
       </div>
